76ed9611d7aec17d0411dfb8768a152c10c7cb81
saikat-007 <85753752+saikat-007@users.noreply.github.com>
2022-01-04 12:19:57 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (31.93%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -8,8 +8,7 @@ public:
             int c = log2(n)+1; // This will count number of bits in the given number(i.e binary of 7 is 111 so it will return 3)
 
             int num=pow(2,c)-1; // returns the max binary number on given number of digits
-            int res= num^n; // xor with num will give us desired output(5=101=> 101 ^ 111 = 010 => 2)
-            return res;
+            return num^n; // xor with num will give us desired output(5=101=> 101 ^ 111 = 010 => 2)
         }
         
     }
