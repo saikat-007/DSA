22c467507b79b7770df805af8d38f83730f7a63a
saikat-007 <85753752+saikat-007@users.noreply.github.com>
2022-01-04 12:11:05 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (5.99%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n==0)
+            return 1;
+        else
+        {
+            int c = log2(n)+1; // This will count number of bits in the given number(i.e binary of 7 is 111 so it will return 3)
+
+            int num=pow(2,c)-1; // returns the max binary number on given number of digits
+            int res= num^n; // xor with num will give us desired output(5=101=> 101 ^ 111 = 010 => 2)
+            return res;
+        }
+        
+    }
+};
