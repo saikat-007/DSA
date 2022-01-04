9e6fc8a452a7ad122880dbfb02d7548d046e3e5a
saikat-007 <85753752+saikat-007@users.noreply.github.com>
2022-01-04 12:15:21 +0530

    Update 997-find-the-town-judge.cpp

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -14,15 +14,5 @@ public:
                 return i;
         }
         return -1;
-        // vector<int> Trusted(N + 1, 0);
-        // for(auto person : trust){
-        //     Trusted[person[0]]--;
-        //     Trusted[person[1]]++;
-        // }
-        // for(int i = 1;i <= N;i++){
-        //     if(Trusted[i] == N - 1)
-        //         return i;
-        // }
-        // return -1;
     }
 };
