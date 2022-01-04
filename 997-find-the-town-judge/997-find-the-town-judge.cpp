41ca7cb70e9e3f24838df44274318e26795e7b74
saikat-007 <85753752+saikat-007@users.noreply.github.com>
2022-01-04 10:43:11 +0530

    Time: 315 ms (6.59%), Space: 60.8 MB (78.22%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int findJudge(int N, vector<vector<int>>& trust) 
+    int findJudge(int n, vector<vector<int>>& trust) 
     {
-        // vector<int>v(n+1,0);
-        // for(int i=0;i<trust.size();i++)
-        // {
-        //     v[trust[i][0]]--;
-        //     v[trust[i][1]]++;
-        // }
-        // for(int i=0;i<=n+1;i++)
-        // {
-        //     if(v[i] == n-1)
-        //         return i;
-        // }
-        // return -1;
-        vector<int> Trusted(N + 1, 0);
-        for(auto person : trust){
-            Trusted[person[0]]--;
-            Trusted[person[1]]++;
+        vector<int>v(n+1,0);
+        for(int i=0;i<trust.size();i++)
+        {
+            v[trust[i][0]]--;
+            v[trust[i][1]]++;
         }
-        for(int i = 1;i <= N;i++){
-            if(Trusted[i] == N - 1)
+        for(int i=1;i<=n;i++)
+        {
+            if(v[i] == n-1)
                 return i;
         }
         return -1;
+        // vector<int> Trusted(N + 1, 0);
+        // for(auto person : trust){
+        //     Trusted[person[0]]--;
+        //     Trusted[person[1]]++;
+        // }
+        // for(int i = 1;i <= N;i++){
+        //     if(Trusted[i] == N - 1)
+        //         return i;
+        // }
+        // return -1;
     }
 };
