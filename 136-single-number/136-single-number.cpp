143c01c82276b0f865e099c55b618bb52eccdb95
Saikat Banerjee <85753752+saikat-007@users.noreply.github.com>
2022-01-07 21:20:47 +0530

    Time: 41 ms (10.50%), Space: 16.9 MB (84.45%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int res=0;
-        int i=0;
-        for(i=0;i<nums.size()-1;i+=2)
+    int singleNumber(vector<int>& n) {
+        // sort(nums.begin(),nums.end());
+        // int res=0;
+        // int i=0;
+        // for(i=0;i<nums.size()-1;i+=2)
+        // {
+        //     if(nums[i]!=nums[i+1])
+        //          break;
+        // }
+        // return nums[i];
+        
+        //We can use bitwise XOR operator to get desired output in a more optimised way as XOR with same number cancels out so the only single number will leave behind.
+        
+        int num=n[0];
+        for(int i=1;i<n.size();i++)
         {
-            if(nums[i]!=nums[i+1])
-                 break;
+            num^=n[i];
         }
-        return nums[i];
+        return  num;
     }
 };
