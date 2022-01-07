8217245a879ce4d7f8a44c8df629245b43b6f5d5
Saikat Banerjee <85753752+saikat-007@users.noreply.github.com>
2022-01-07 22:17:05 +0530

    Time: 26 ms (21.89%), Space: 19.7 MB (50.11%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& n) {
+        
+        //using Moore's Voting algo
+        
+        int res=0,count=1;
+        
+        // This loop is to find that number whose probability of becoming majority is maximum
+        for(int i=1;i<n.size();i++)
+        {
+            if(n[res] == n[i])
+                count++;
+            
+            else
+                count--;
+            
+            if(count == 0)
+            {
+                res=i;
+                count=1;
+            }
+        }
+        
+        count=0;
+        
+        //This loop is to check if our most probable element is really majority or not?
+        for(int i=0;i<n.size();i++)
+        {
+            if(n[i] == n[res])
+                count++;
+        }
+        if(count <= n.size()/2)
+            return -1;
+        else
+            return n[res];
+    }
+};
