83d39d34955a394686101e5f035e002a999c794c
Saikat Banerjee <85753752+saikat-007@users.noreply.github.com>
2022-02-02 21:54:23 +0530

    Time: 30 ms (50.21%), Space: 24 MB (81.57%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -2,24 +2,41 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums)
     {
+//         int n=nums.size();
+//         vector<int>right(n); // to store the product of all the elements on the right side of it and this also stores the final result.
+        
+//         int left=1; // we can make a vector like right to store product of all the elements on the left side of it but insted we can maintain a variable to save space
+        
+//         right[n-1]=1;  //no element on right
+        
+//         for(int i=n-2 ; i >= 0 ; i--)
+//         {
+//             right[i]=right[i+1]*nums[i+1];    
+//         }
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             right[i] = right[i]*left;   //updating right vector for final answer
+//             left=left*nums[i];
+//         }
+        
+//         return right;
+        
+        
         int n=nums.size();
-        vector<int>right(n); // to store the product of all the elements on the right side of it and this also stores the final result.
+        vector<int>right(n);
+        int left =1;
         
-        int left=1; // we can make a vector like right to store product of all the elements on the left side of it but insted we can maintain a variable to save space
-        
-        right[n-1]=1;  //no element on right
-        
-        for(int i=n-2 ; i >= 0 ; i--)
+        right[n-1]=1;
+        for(int i=n-2;i>=0;i--)
         {
-            right[i]=right[i+1]*nums[i+1];    
+            right[i] = nums[i+1]*right[i+1];
         }
-        
         for(int i=0;i<n;i++)
         {
-            right[i] = right[i]*left;   //updating right vector for final answer
+            right[i]=right[i]*left;
             left=left*nums[i];
         }
-        
         return right;
     }
 };
