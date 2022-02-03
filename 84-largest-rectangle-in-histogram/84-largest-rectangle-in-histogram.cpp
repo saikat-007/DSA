b3f844ae6a1747785f5b7f80cf907f8d168ecb3b
Saikat Banerjee <85753752+saikat-007@users.noreply.github.com>
2022-02-03 12:58:27 +0530

    Time: 201 ms (41.88%), Space: 66.5 MB (25.18%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -6,36 +6,88 @@ public:
         vector<int>left(n);
         vector<int>right(n);
         stack<int>stk;
+        
+        
+        // for finding left smaller element 
+        
         for(int i=0;i<n;i++)
         {
-            while(stk.empty()==false && height[stk.top()] >= height[i])
+            while(stk.empty()==false && height[stk.top()]>=height[i])
             {
                 stk.pop();
             }
-            if(stk.empty()==true)left[i]=0;
-            else left[i]=stk.top()+1;
+            
+            if(stk.empty())
+                left[i]=0;
+            else
+                left[i] = stk.top()+1;
+            
             stk.push(i);
         }
         
-        while(!stk.empty()) // for reusing the stack
+        //emptying stack stk to reuse
+        
+        while(stk.empty()!=true)
+        {
             stk.pop();
+        }
+        
+        // finding right smaller
         
         for(int i=n-1;i>=0;i--)
         {
-            while(!stk.empty() && height[stk.top()]>=height[i])
+            while(stk.empty()==false && height[stk.top()]>=height[i])
             {
                 stk.pop();
             }
-            if(stk.empty()) right[i]=n-1;
-            else right[i]=stk.top()-1;
+            
+            if(stk.empty())
+                right[i]=n-1;
+            else
+                right[i]=stk.top()-1;
+            
             stk.push(i);
         }
         
+        //finding area
         int max_area=0;
         for(int i=0;i<n;i++)
         {
-            max_area=max(max_area,(height[i]*(right[i]-left[i]+1)));
+            max_area=max(max_area,(right[i]-left[i] + 1)*height[i]);
         }
+        
         return max_area;
     }
 };
+
+// for(int i=0;i<n;i++)
+//         {
+//             while(stk.empty()==false && height[stk.top()] >= height[i])
+//             {
+//                 stk.pop();
+//             }
+//             if(stk.empty()==true)left[i]=0;
+//             else left[i]=stk.top()+1;
+//             stk.push(i);
+//         }
+        
+//         while(!stk.empty()) // for reusing the stack
+//             stk.pop();
+        
+//         for(int i=n-1;i>=0;i--)
+//         {
+//             while(!stk.empty() && height[stk.top()]>=height[i])
+//             {
+//                 stk.pop();
+//             }
+//             if(stk.empty()) right[i]=n-1;
+//             else right[i]=stk.top()-1;
+//             stk.push(i);
+//         }
+        
+//         int max_area=0;
+//         for(int i=0;i<n;i++)
+//         {
+//             max_area=max(max_area,(height[i]*(right[i]-left[i]+1)));
+//         }
+//         return max_area;
