57049ae9cea3605a96df3ae263a71d2615556da1
Saikat Banerjee <85753752+saikat-007@users.noreply.github.com>
2022-02-07 20:33:15 +0530

    Time: 4 ms (67.41%), Space: 6.7 MB (67.00%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -2,23 +2,34 @@ class Solution {
 public:
     char findTheDifference(string s, string t)
     {
-        int visited[26]={0};
+//         int visited[26]={0};
+//         for(int i=0;i<t.size();i++)
+//         {
+//             visited[t[i]-'a']++;
+//         }
+        
+//         for(int i=0;i<s.size();i++)
+//         {
+//              visited[s[i]-'a']--;
+//         }
+        
+//         int res;
+//         for(int i=0;i<26;i++)
+//         {
+//             if(visited[i]!=0)
+//                 res = i;
+//         }
+//         return res+'a';
+        
+        int sum1=0,sum2=0;
         for(int i=0;i<t.size();i++)
         {
-            visited[t[i]-'a']++;
+            sum1+=(t[i]-'a');
         }
-        
         for(int i=0;i<s.size();i++)
         {
-             visited[s[i]-'a']--;
+            sum2+=(s[i]-'a');
         }
-        
-        int res;
-        for(int i=0;i<26;i++)
-        {
-            if(visited[i]!=0)
-                res = i;
-        }
-        return res+'a';
+        return (sum1-sum2)+'a';
     }
 };
