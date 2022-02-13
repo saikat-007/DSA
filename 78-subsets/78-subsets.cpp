1664d12df212ab98a7f931fde44e5fbea678d515
Saikat Banerjee <85753752+saikat-007@users.noreply.github.com>
2022-02-13 23:12:16 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (85.44%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,33 +1,67 @@
 class Solution {
 public:
-    void get_subset(vector<int>& nums,int idx,vector<vector<int>>&result)
-    {
-        if(idx == nums.size()-1)
-        {
-            result.push_back({});
-            result.push_back({nums[idx]});
-        }
+//     void get_subset(vector<int>& nums,int idx,vector<vector<int>>&result)
+//     {
+//         if(idx == nums.size()-1)
+//         {
+//             result.push_back({});
+//             result.push_back({nums[idx]});
+//         }
         
-        else
+//         else
+//         {
+//             get_subset(nums,idx+1,result);
+//             int n=result.size();
+//             for(int i=0;i<n;i++)
+//             {
+//                 vector<int>r=result[i];
+//                 r.push_back(nums[idx]);
+//                 result.push_back(r);
+//             }
+//         }
+//     }
+    
+    vector<vector<int>> subsets(vector<int>& nums)
+    {
+//         if(nums.size()==0)
+//             return {{}};
+        
+//         vector<vector<int>>res;
+//         get_subset(nums,0,res);
+//         return res;
+        
+        if(nums.empty())
+            return {{}};
+        
+        vector<vector<int>>result={{}};
+        for(int x:nums)
         {
-            get_subset(nums,idx+1,result);
             int n=result.size();
             for(int i=0;i<n;i++)
             {
                 vector<int>r=result[i];
-                r.push_back(nums[idx]);
+                r.push_back(x);
                 result.push_back(r);
             }
         }
-    }
-    
-    vector<vector<int>> subsets(vector<int>& nums)
-    {
-        if(nums.size()==0)
-            return {{}};
         
-        vector<vector<int>>res;
-        get_subset(nums,0,res);
-        return res;
+        return result;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
