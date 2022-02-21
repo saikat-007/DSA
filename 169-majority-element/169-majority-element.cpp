e9b571832c3356f20ec03332210778d6de72be16
Saikat Banerjee <85753752+saikat-007@users.noreply.github.com>
2022-02-21 23:39:19 +0530

    Time: 27 ms (50.68%), Space: 19.6 MB (88.96%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -7,24 +7,25 @@ public:
         int res=0,count=1;
         
         // This loop is to find that number whose probability of becoming majority is maximum
-        for(int i=1;i<n.size();i++)
-        {
-            if(n[res] == n[i])
-                count++;
-            
-            else
-                count--;
-            
-            if(count == 0)
-            {
-                res=i;
-                count=1;
-            }
-        }
+       for(int i=0;i<n.size();i++)
+       {
+           if(n[res] == n[i])
+               count++;
+           
+           else
+               count--;
+           
+           if(count == 0)
+           {
+               res=i;
+               count=1;
+           }
+       }
+        
+        //This loop is to check if our most probable element is really majority or not?
         
         count=0;
         
-        //This loop is to check if our most probable element is really majority or not?
         for(int i=0;i<n.size();i++)
         {
             if(n[i] == n[res])
