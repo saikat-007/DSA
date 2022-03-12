b07fa9e50d872c9edb3f26cb4b3fc4efe62dcdc5
Saikat Banerjee <85753752+saikat-007@users.noreply.github.com>
2022-03-12 23:59:18 +0530

    Time: 12 ms (58.04%), Space: 11.2 MB (87.91%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -18,71 +18,68 @@ class Solution {
 public:
     Node* copyRandomList(Node* head)
     {
-//         unordered_map<Node*,Node*>mp;
-//         Node*p=head;
+
+        unordered_map<Node*,Node*>mp;
         
-//         //creating the deep copies of nodes...without links
+        Node*p=head;
         
-//         while(p!=NULL)
+        while(p!=NULL)
+        {
+            mp[p] = new Node(p->val);
+            p=p->next;
+        }
+        
+        p=head;
+        while(p!=NULL)
+        {
+            mp[p]->next = mp[p->next];
+            mp[p]->random = mp[p->random];
+            p=p->next;
+        }
+        
+        return mp[head];
+        
+//         //O(1) space
+        
+        
+//         //step-1 :  make copy of each node,and link them together side-by-side in a single list.
+//         Node*it=head;
+//         Node*front=head;
+        
+//         while(it!=NULL)
 //         {
-//             mp[p]=new Node(p->val);
-//             p=p->next;
+//             front=it->next;
+//             Node*dc=new Node(it->val);
+//             it->next=dc;
+//             dc->next=front;
+//             it=front;
 //         }
         
-//         //connecting links
+//         //step-2: assign random pointers for the copy nodes.
         
-//         Node*ptr=head;
-//         while(ptr!=NULL)
+//         it=head;
+//         while(it!=NULL)
 //         {
-//             mp[ptr]->next=mp[ptr->next];
-//             mp[ptr]->random=mp[ptr->random];
-//             ptr=ptr->next;
+//             if(it->random!=NULL)
+//             {
+//                 it->next->random=it->random->next;
+//             }
+//             it=it->next->next;
 //         }
         
-//         return mp[head];
+//         // step-3: restore the original list, and extract the copy list.
+//         it=head;
+//         Node*dummy=new Node{0};
+//         Node*cpy=dummy;
         
-        
-        //O(1) space
-        
-        
-        //step-1 :  make copy of each node,and link them together side-by-side in a single list.
-        Node*it=head;
-        Node*front=head;
-        
-        while(it!=NULL)
-        {
-            front=it->next;
-            Node*dc=new Node(it->val);
-            it->next=dc;
-            dc->next=front;
-            it=front;
-        }
-        
-        //step-2: assign random pointers for the copy nodes.
-        
-        it=head;
-        while(it!=NULL)
-        {
-            if(it->random!=NULL)
-            {
-                it->next->random=it->random->next;
-            }
-            it=it->next->next;
-        }
-        
-        // step-3: restore the original list, and extract the copy list.
-        it=head;
-        Node*dummy=new Node{0};
-        Node*cpy=dummy;
-        
-        while(it!=NULL)
-        {
-            front=it->next->next;
-            cpy->next=it->next;
-            cpy=cpy->next;
-            it->next=front;
-            it=it->next;
-        }
-        return dummy->next;
+//         while(it!=NULL)
+//         {
+//             front=it->next->next;
+//             cpy->next=it->next;
+//             cpy=cpy->next;
+//             it->next=front;
+//             it=it->next;
+//         }
+//         return dummy->next;
     }
 };
