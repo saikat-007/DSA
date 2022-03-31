bd58b3cdb9ea8ff875a3e71b67281bf7e2507a99
Saikat Banerjee <85753752+saikat-007@users.noreply.github.com>
2022-03-31 21:12:02 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (41.56%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,67 +1,35 @@
 class Solution {
 public:
-//     void get_subset(vector<int>& nums,int idx,vector<vector<int>>&result)
-//     {
-//         if(idx == nums.size()-1)
-//         {
-//             result.push_back({});
-//             result.push_back({nums[idx]});
-//         }
-        
-//         else
-//         {
-//             get_subset(nums,idx+1,result);
-//             int n=result.size();
-//             for(int i=0;i<n;i++)
-//             {
-//                 vector<int>r=result[i];
-//                 r.push_back(nums[idx]);
-//                 result.push_back(r);
-//             }
-//         }
-//     }
+    void generate_subset(vector<int>& nums , int idx , vector<vector<int>>& result)
+    {
+        int n = nums.size();
+        if(idx == n-1)
+        {
+            result.push_back({});
+            result.push_back({nums[idx]});
+        }
+        else
+        {
+            generate_subset(nums , idx+1 , result);
+            int size = result.size();
+            
+            for(int i = 0 ; i  < size ; i++)
+            {
+                vector<int>ans = result[i];
+                ans.push_back(nums[idx]);
+                result.push_back(ans);
+            }
+        }
+    }
     
     vector<vector<int>> subsets(vector<int>& nums)
     {
-//         if(nums.size()==0)
-//             return {{}};
-        
-//         vector<vector<int>>res;
-//         get_subset(nums,0,res);
-//         return res;
-        
         if(nums.empty())
             return {{}};
         
-        vector<vector<int>>result={{}};
-        for(int x:nums)
-        {
-            int n=result.size();
-            for(int i=0;i<n;i++)
-            {
-                vector<int>r=result[i];
-                r.push_back(x);
-                result.push_back(r);
-            }
-        }
+        vector<vector<int>>result;
+        generate_subset(nums , 0 , result);
         
         return result;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
