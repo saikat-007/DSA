22d85bea96fb765d1ded61b322a179598e3b4059
Saikat Banerjee <85753752+saikat-007@users.noreply.github.com>
2022-05-12 21:43:33 +0530

    Time: 0 ms (100.00%), Space: 8 MB (28.13%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,35 +1,99 @@
 class Solution {
 public:
-    void recPermute(vector<int>&nums , vector<int>&ds , vector<vector<int>>&res , int freq[])
-    {
-        if(ds.size() == nums.size())
-        {
-            res.push_back(ds);
-            return;
-        }
+    
+    // approach 1
+    
+//     void getPermutation(vector<vector<int>>&res , vector<int>&ds , vector<int>&nums , vector<int>&count)
+//     {
+//         if(ds.size() == nums.size())
+//         {
+//             res.push_back(ds);
+//             return;
+//         }
+    
+//         for(int i = 0 ; i < nums.size() ; i++)
+//         {
+//             if(count[i] == 0)
+//             {
+//                 count[i] = 1;
+//                 ds.push_back(nums[i]);
+//                 getPermutation(res,ds,nums,count);
+//                 ds.pop_back();
+//                 count[i] = 0;
+//             }
+//         }
+//     }
+    
+//     vector<vector<int>> permute(vector<int>& nums)
+//     {
+//         int n = nums.size();
+//         vector<int>visited(n,0);
         
-        for(int i = 0 ; i < nums.size() ; i++)
-        {
-            if(freq[i] == 0)
-            {
-                freq[i] = 1;
-                ds.push_back(nums[i]);
-                recPermute(nums , ds , res , freq);
-                freq[i] = 0;
-                ds.pop_back();
-            }
-        }
+//         vector<vector<int>>res;
+//         vector<int>ds;
+        
+//         getPermutation(res,ds,nums,visited);
+//         return res;
+//     }
+    
+    
+    //approach 2 : find the permutation for the smaller subarray and append the element which was left to the beginning
+    
+//     vector<vector<int>> perm(vector<int>nums)
+//     {
+//         if(nums.size() == 0)
+//             return {{}};
+        
+//         vector<vector<int>>ans;
+//         for(int i = 0 ; i < nums.size() ; i++)
+//         {
+//             vector<int>nums2;
+//             for(int j = 0 ; j < nums.size() ; j++)
+//             {
+//                 if(i!=j)
+//                 {
+//                     nums2.push_back(nums[j]);
+//                 }
+//             }
+            
+//             vector<vector<int>>temp = perm(nums2);
+            
+//             for(auto x : temp)
+//             {
+//                 x.push_back(nums[i]);
+//                 ans.push_back(x);
+//             }
+//         }
+        
+//         return ans;
+//     }
+//     vector<vector<int>> permute(vector<int>& nums)
+//     {
+//         return perm(nums);
+//     }
+    
+    //approach 3:
+    
+    vector<vector<int>>ans;
+    void perm(vector<int>&nums , int i)
+    {
+       if(i == nums.size()) 
+       {
+           ans.push_back(nums);
+           return;
+       }
+        
+       for(int j = i ; j < nums.size() ; j++)
+       {
+           swap(nums[i],nums[j]);
+           perm(nums,i+1);
+           swap(nums[i],nums[j]);
+       }
     }
     vector<vector<int>> permute(vector<int>& nums)
     {
-        vector<vector<int>>res;
-        vector<int>ds;
-        int freq[nums.size()];
-        for(int i = 0 ; i  < nums.size() ; i++)
-        {
-            freq[i] = 0;
-        }
-        recPermute(nums,ds,res,freq);
-        return res;
+        perm(nums,0);
+        return ans;
     }
+    
 };
