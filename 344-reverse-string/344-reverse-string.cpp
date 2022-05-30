5392e934beaf0081d50136dec1c966ab49096332
Saikat Banerjee <85753752+saikat-007@users.noreply.github.com>
2022-05-30 11:31:54 +0530

    Time: 31 ms (44.89%), Space: 23.2 MB (76.63%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s)
+    {
+        int low = 0 , high = s.size() - 1;
+        while(low <= high)
+        {
+            swap(s[low] , s[high]);
+            low++;
+            high--;
+        }
+    }
+};
