c0e492925700ea04f752466ada2b348dbe59e60d
Saikat Banerjee <85753752+saikat-007@users.noreply.github.com>
2022-06-10 12:11:09 +0530

    Time: 35 ms (38.39%), Space: 10.9 MB (26.11%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -2,24 +2,28 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s)
     {
-        unordered_map<char,int>mpp;
-        int l=0,r=0;
-        int length=0;
-        int n=s.size();
+        int length = 0 , l = 0 , r = 0;
+        unordered_set<char>st;
+        int n = s.size();
         
-        while(r<n)
+        while(r < n)
         {
-            if(mpp.find(s[r])!=mpp.end())
+            if(st.find(s[r]) == st.end())
+                st.insert(s[r]);
+            
+            else
             {
-                l=max(mpp[s[r]]+1,l);  //if the current element is already present in the map and its previous index is greater than current index of l then we update the left pointer to (its previous index + 1) else we don't change l pointer
+                while(st.find(s[r]) != st.end())
+                {
+                    st.erase(s[l++]);
+                }
+                st.insert(s[r]);
             }
             
-            mpp[s[r]]=r; // updating the index of current element
-            
-            length=max(r-l+1,length); //r-l+1 is the length of the string in which there is no repeating characters
-            
+            length = max(length , r - l + 1);
             r++;
         }
+        
         return length;
     }
 };
