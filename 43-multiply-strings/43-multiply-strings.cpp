c7c37c7ce2b4c9af594b48b6df5e9c373da61b0a
Saikat Banerjee <85753752+saikat-007@users.noreply.github.com>
2022-07-05 00:17:17 +0530

    Time: 14 ms (46.03%), Space: 6.8 MB (62.52%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.cpp
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -15,15 +15,23 @@ public:
             for(int j = 0 ; j < num2.size() ; j++)
             {
                 int digit = (num1[i] - '0') * (num2[j] - '0');
-                ans[i+j] += digit;
-                ans[i+j+1] += ans[i+j] / 10;
-                ans[i+j] = ans[i+j] % 10;
+                ans[i+j] += digit;//adding the obtained result to its correct place 
+				//we increase the position in ans as we move in num1 and num2 
+				//because that is how we multiply 2 numbers
+				//123
+				//x456
+				// we first multiply 3 and 6 and add their result at units place which 
+				//is 0(of num1)+0(of num2) th position the multiply 6 and 2 and add result in 0+1 th position
+                ans[i+j+1] += ans[i+j] / 10; //taking care of carry
+                ans[i+j] = ans[i+j] % 10; // modding the original result by 10 to keep our answer in bound
             }
         }
         
         reverse(ans.begin(),ans.end());
         string res="";
         int i=0;
+        
+        //to avoid leading 0`s
         while(i < ans.size() && ans[i] == 0)
         {
             i++;
