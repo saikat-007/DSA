96db2b0cc29095f1abef6741eb58fbdfe728925e
Saikat Banerjee <85753752+saikat-007@users.noreply.github.com>
2022-08-09 21:20:05 +0530

    Time: 2 ms (42.68%), Space: 6.2 MB (19.82%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
-    int get(int n,vector<int>&dp)
-    {
-        if(n == 0 )
-            return 0;
+//     int get(int n,vector<int>&dp)
+//     {
+//         if(n == 0 )
+//             return 0;
         
-        if(n == 1 || n == 2)    
-            return 1;
+//         if(n == 1 || n == 2)    
+//             return 1;
         
-        if(dp[n] != -1)
-            return dp[n];
+//         if(dp[n] != -1)
+//             return dp[n];
         
-        else
-            return dp[n] = get(n-1,dp) + get(n-2,dp) + get(n-3,dp);
-    }
+//         else
+//             return dp[n] = get(n-1,dp) + get(n-2,dp) + get(n-3,dp);
+//     }
+    
+//     int tribonacci(int n)
+//     {
+//         vector<int>dp(n+1,-1);
+//         return get(n,dp);
+//     }
     
     int tribonacci(int n)
     {
-        vector<int>dp(n+1,-1);
-        return get(n,dp);
+        if(n == 0 || n == 1)
+            return n;
+        
+        if(n == 2)
+            return 1;
+        
+        vector<int>dp(n+1);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        
+        for(int i = 3 ; i <= n ; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        
+        return dp[n];
     }
 };
