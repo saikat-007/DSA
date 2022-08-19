83c21c3f5733c44d08944514393dc2d89bad91c6
Saikat Banerjee <85753752+saikat-007@users.noreply.github.com>
2022-08-19 21:48:16 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (37.89%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -18,29 +18,53 @@
 //     }
 // };
 
+//<---------------------------------- Memoisation ------------------------------------------>
+
+// class Solution {
+// public:
+//     int getCount(int n , vector<int>&dp)
+//     {
+//         if(n == 0 || n == 1)    
+//             return dp[n] = 1;
+        
+//         int count = 0;
+        
+//         if(dp[n] != -1)
+//             return dp[n];
+        
+//         for(int i = 1 ; i <= n ; i++)
+//         {
+//             count += (getCount(i-1 , dp)*getCount(n-i , dp));
+//         }
+        
+//         return dp[n] = count;
+//     }
+    
+//     int numTrees(int n)
+//     {
+//         vector<int>dp(n+1,-1);
+//         return getCount(n , dp);
+//     }
+// };
+
+//<-------------------------------------- Tabulation ----------------------------------------->
+
 class Solution {
 public:
-    int getCount(int n , vector<int>&dp)
-    {
-        if(n == 0 || n == 1)    
-            return dp[n] = 1;
-        
-        int count = 0;
-        
-        if(dp[n] != -1)
-            return dp[n];
-        
-        for(int i = 1 ; i <= n ; i++)
-        {
-            count += (getCount(i-1 , dp)*getCount(n-i , dp));
-        }
-        
-        return dp[n] = count;
-    }
-    
     int numTrees(int n)
     {
-        vector<int>dp(n+1,-1);
-        return getCount(n , dp);
+       vector<int> dp(n+1,0);
+        dp[0] = dp[1] = 1;
+        
+        // i -> node
+        for(int i=2;i<=n;i++){
+            int ans = 0;
+            for(int j=1;j<=i;j++){
+                ans += dp[j-1] * dp[i-j];
+            }
+            dp[i] = ans;
+        }
+        
+        return dp[n];
     }
-};
+ };
