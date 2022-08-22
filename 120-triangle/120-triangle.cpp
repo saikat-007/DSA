e9576bb73f880301d140cd1ebc842470407fb1bc
Saikat Banerjee <85753752+saikat-007@users.noreply.github.com>
2022-08-22 21:50:04 +0530

    Time: 6 ms (85.77%), Space: 9 MB (31.66%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,44 +1,22 @@
 class Solution {
 public:
-    
-//     int minsum = INT_MAX;
-    
-//     void getTotal(int i , int j ,vector<vector<int>>& triangle , int sum)
-//     {
-//         if(i == triangle.size())
-//         {
-//             minsum = min(sum , minsum);
-//             return;
-//         }
+    int getTotal(int idx , int row , vector<vector<int>>& triangle , vector<vector<int>>& dp)
+    {
+        if(row == triangle.size() - 1)
+            return triangle[row][idx];
         
-//         sum += triangle[i][j];
-//         getTotal(i+1 , j , triangle , sum);
-//         getTotal(i+1 , j+1 , triangle , sum);
-//     }
-    
-   
-//     int minimumTotal(vector<vector<int>>& triangle)
-//     {
-//         int sum = 0 , i = 0 , j = 0;
-//         getTotal(i,j,triangle,sum);
+        if(dp[row][idx] != -1)
+            return dp[row][idx];
         
-//         return minsum;
-//     }
+        else
+            return dp[row][idx] = triangle[row][idx] + min(getTotal(idx , row + 1 , triangle , dp) , getTotal(idx + 1, row + 1 , triangle , dp));
+    }
     
-    
-       int solve(int i, int j, int n, vector<vector<int>>&triangle, vector<vector<int>>&dp) {
-		if(i == n - 1)
-			return triangle[i][j];
-		if(dp[i][j] != -1)
-			return dp[i][j];
-		int bottom = triangle[i][j] + solve(i+1, j, n, triangle, dp);
-		int  bottom_right = triangle[i][j] + solve(i+1, j+1, n, triangle, dp);
-		return dp[i][j] = min(bottom, bottom_right);
-	    }
-    
-	    int minimumTotal(vector<vector<int>>& triangle) {
-		int n = triangle.size();
-		vector<vector<int>>dp(n, vector<int>(n, -1));
-		return solve(0, 0, n, triangle, dp);
-	}
+    int minimumTotal(vector<vector<int>>& triangle)
+    {
+        int idx = 0 , row = 0;
+        int n = triangle.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return getTotal(idx,row,triangle,dp);
+    }
 };
