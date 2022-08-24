015676e4c0a2f6b69036974be1689933464febf1
Saikat Banerjee <85753752+saikat-007@users.noreply.github.com>
2022-08-25 00:49:46 +0530

    Time: 15 ms (57.19%), Space: 10.1 MB (30.54%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int solveRec(int i , int j , vector<vector<int>>& grid, int m , int n , vector<vector<int>> &dp)
+    int getsum(int i , int j , vector<vector<int>>& grid , int m , int n , vector<vector<int>> &dp)
     {
         if(i == m-1 && j == n-1)
-            return grid[m-1][n-1];
+            return grid[i][j];
         
-        if(i > m-1 || j > n-1)
+        if(i > m-1 ||  j > n-1)
             return INT_MAX;
         
-        if(dp[i][j]!=-1)
+        if(dp[i][j] != -1)
             return dp[i][j];
         
-        else
-            return dp[i][j] = min(solveRec(i+1,j,grid,m,n,dp) , solveRec(i,j+1,grid,m,n,dp)) + grid[i][j];
+        return dp[i][j] = grid[i][j] + min(getsum(i+1,j,grid,m,n,dp),getsum(i,j+1,grid,m,n,dp));
     }
     
     int minPathSum(vector<vector<int>>& grid)
@@ -20,7 +19,6 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        
-        return solveRec(0,0,grid,m,n,dp);
+        return getsum(0,0,grid,m,n,dp);
     }
 };
