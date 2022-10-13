6e9ec02267d798ac4163f6644cb5f70ada18ff7d
Saikat Banerjee <85753752+saikat-007@users.noreply.github.com>
2022-10-13 23:54:31 +0530

    Time: 29 ms (37.79%), Space: 7.7 MB (37.95%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -10,17 +10,21 @@ class Solution {
 public:
     void deleteNode(ListNode* node)
     {
-        ListNode* p = node;
-        ListNode* q = node -> next;
+//         ListNode* p = node;
+//         ListNode* q = node -> next;
         
-        while(q != NULL)    
-        {
-            node -> val = q -> val;
-            p = node;
-            node = node -> next;
-            q = q -> next;
-        }
+//         while(q != NULL)    
+//         {
+//             node -> val = q -> val;
+//             p = node;
+//             node = node -> next;
+//             q = q -> next;
+//         }
         
-        p -> next = 0;
+//         p -> next = 0;
+        
+        ListNode * p = node;
+        p -> val = p -> next -> val;
+        p -> next = p -> next -> next;
     }
 };
