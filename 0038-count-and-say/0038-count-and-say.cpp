592ec30698aacbd58d46678b756ef356e1c614ef
Saikat Banerjee <85753752+saikat-007@users.noreply.github.com>
2022-10-17 21:33:46 +0530

    Time: 6 ms (84.65%), Space: 6.6 MB (77.44%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string countAndSay(int n)
+    {
+        if(n == 1)
+            return "1";
+        
+        string temp = countAndSay(n-1);
+        string str = "";
+        
+        int i = 0;
+        while(i < temp.size())
+        {
+            char c = temp[i];
+            int count = 0;
+            while(temp[i] == c)
+            {
+                count++;
+                i++;
+            }
+            str += count + '0';
+            str += c;
+        }
+        
+        return str;
+    }
+};
