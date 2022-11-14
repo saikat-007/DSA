44e3043657c94980d1cd4956c8503205476a8c91
Saikat Banerjee <85753752+saikat-007@users.noreply.github.com>
2022-11-14 21:18:39 +0530

    Time: 20 ms (66.67%), Space: 9.4 MB (83.33%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -27,28 +27,73 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int mod = 1e9 + 7;
+//     int getcount(vector<int>&dp , int zero , int one , int n)
+//     {
+//         if(n == 0) return 1;
+//         if(n < 0) return 0;
+        
+//         if(dp[n] != -1) return dp[n];
+        
+//         return dp[n] = (getcount(dp,zero,one,n-zero) % mod + getcount(dp,zero,one,n-one) % mod) % mod;
+//     }
+    
+//     int countGoodStrings(int low, int high, int zero, int one)
+//     {
+//         int ans = 0;
+//         vector<int>dp(high+1,-1);
+        
+//         for(int i = low ; i <= high ; i++)
+//         {
+//             ans = (ans + getcount(dp , zero , one , i) % mod) % mod;
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int mod = 1e9 + 7;
-    int getcount(vector<int>&dp , int zero , int one , int n)
-    {
-        if(n == 0) return 1;
-        if(n < 0) return 0;
-        
-        if(dp[n] != -1) return dp[n];
-        
-        return dp[n] = (getcount(dp,zero,one,n-zero) % mod + getcount(dp,zero,one,n-one) % mod) % mod;
-    }
-    
     int countGoodStrings(int low, int high, int zero, int one)
     {
-        int ans = 0;
-        vector<int>dp(high+1,-1);
+        vector<int>dp(high+1 , 0);
+        dp[0] = 1;
+        for(int k = 1 ; k <= high ; k++)
+        {
+            if(k >= zero) dp[k] += dp[k-zero] % mod;
+            if(k >= one) dp[k] += dp[k-one] % mod;
+        }
         
+        long long count = 0;
         for(int i = low ; i <= high ; i++)
         {
-            ans = (ans + getcount(dp , zero , one , i) % mod) % mod;
+            count = (count + dp[i]) % mod;
         }
-        return ans;
+        
+        return count;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
